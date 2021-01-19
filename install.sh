28840bd585c2cc620bfe2f01ccd5648fa349f18e
Abir Majumdar <amajumdar@newrelic.com>
2021-01-19 13:40:52 -0500

    Script fix

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 yum install wget git nano -y
 wget https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.1%2B9/OpenJDK15U-jdk_x64_linux_hotspot_15.0.1_9.tar.gz
 mkdir /opt/jdk
-tar xvf OpenJDK15U-jdk_x64_linux_hotspot_15.0.1_9.tar.gz  -C /opt/jdk/
+tar xvf OpenJDK15U-jdk_x64_linux_hotspot_15.0.1_9.tar.gz  -C /opt/
+mv /opt/jdk-15.0.1+9 /opt/jdk/
 wget https://www-eu.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 tar xvf apache-maven-3.6.3-bin.tar.gz  -C /usr/lib/
 
