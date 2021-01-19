7b81768d5e24dc50ee3c82c5dd2d6f29c0ef373c
Abir Majumdar <amajumdar@newrelic.com>
2021-01-19 13:59:55 -0500

    Tweaking jvm

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 yum install wget git nano -y
-wget https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.1%2B9/OpenJDK15U-jdk_x64_linux_hotspot_15.0.1_9.tar.gz
-mkdir /opt/jdk
-tar xvf OpenJDK15U-jdk_x64_linux_hotspot_15.0.1_9.tar.gz  -C /opt/
-mv /opt/jdk-15.0.1+9 /opt/jdk/
+yum install java-11-openjdk-devel -y
 wget https://www-eu.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 tar xvf apache-maven-3.6.3-bin.tar.gz  -C /usr/lib/
 
 cat << EOF >> /etc/profile
-JAVA_HOME=/opt/jdk/
+JAVA_HOME=/usr/lib/jvm/java
 export JAVA_HOME
 
 M2_HOME="/usr/lib/apache-maven-3.6.3"
@@ -18,7 +15,7 @@ M2="\$M2_HOME/bin"
 MAVEN_OPTS="-Xms256m -Xmx512m"
 export M2 MAVEN_OPTS
 
-PATH=\$M2:\$PATH:/opt/jdk/bin
+PATH=\$M2:\$PATH:/usr/lib/jvm/java/bin
 export PATH
 EOF
 
